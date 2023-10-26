7b105a4fe9ac83eca4a78418fc3272de083012c9
juandetto <juanmdetto@gmail.com>
2023-10-26 18:55:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // URL de la API de CriptoYa
-    const apiUrl = "https://criptoya.com/api/buenbit/usd/ars/0.1";
+// Variables for easy customization
+const API_URL = "https://criptoya.com/api/usdc/ars/100";
+const PREFERRED_EXCHANGE = "lemoncash";
+// Cambiar arriba por tu exchange preferido (belo, ripio, lemoncash, buenbit, satoshitango, decrypto, letsbit, bybit)
 
-    // Elementos de la página donde se mostrarán los precios de bid y ask
-    const bidPriceElement = document.getElementById("bidPrica");
-    const askPriceElement = document.getElementById("askPrice");
+// Fetch the exchange rate from the API
+async function fetchExchangeRate() {
+    let req = new Request(API_URL);
+    let apiData = await req.loadJSON();
+    return apiData[PREFERRED_EXCHANGE];
+}
 
-    // Realizar la solicitud a la API
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            // Actualizar los elementos de la página con los datos de la API
-            bidPriceElement.textContent = data.bid;
-            askPriceElement.textContent = data.ask;
-        })
-        .catch((error) => {
-            console.error("Error al obtener los datos de la API: ", error);
-        });
-});
+// Create the widget
+async function createWidget(exchangeRate) {
+    let widget = new ListWidget();
+    widget.backgroundColor = new Color("#1E2328");
+    
+    let titleText = "USDC";
+    let title = widget.addText(titleText);
+    title.textColor = new Color("#2975CB");
+    title.font = Font.boldSystemFont(20);
+    
+    let exchangeName = widget.addText(PREFERRED_EXCHANGE.charAt(0).toUpperCase() + PREFERRED_EXCHANGE.slice(1));  // Capitalize the exchange name
+    exchangeName.textColor = Color.gray();
+    exchangeName.font = Font.systemFont(14);
+    
+    widget.addSpacer();
+    
+    let askText = "$" + exchangeRate.ask.toLocaleString('en-US', {minimumFractionDigits: 2});
+    let ask = widget.addText(askText);
+    ask.textColor = Color.white();
+    ask.font = Font.boldSystemFont(20);
+    
+    let totalAskText = "$" + exchangeRate.bid.toLocaleString('en-US', {minimumFractionDigits: 2});
+    let totalAsk = widget.addText(totalAskText);
+    totalAsk.textColor = Color.gray();
+    totalAsk.font = Font.systemFont(14); // Making the font size similar to the exchange name
+    
+    return widget;
+}
+
+// Main
+if (config.runsInWidget) {
+    let exchangeRate = await fetchExchangeRate();
+    let widget = await createWidget(exchangeRate);
+    Script.setWidget(widget);
+    Script.complete();
+} else {
+    // For testing purposes
+    let exchangeRate = await fetchExchangeRate();
+    let widget = await createWidget(exchangeRate);
+    widget.presentSmall();
+}
