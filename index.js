eb8876cb60d3744eb1332704f9e70a7a01633bcd
juandetto <juanmdetto@gmail.com>
2023-10-26 18:52:27 -0300

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // URL de la API de CriptoYa
+    const apiUrl = "https://criptoya.com/api/buenbit/usd/ars/0.1";
+
+    // Elementos de la página donde se mostrarán los precios de bid y ask
+    const bidPriceElement = document.getElementById("bid");
+    const askPriceElement = document.getElementById("ask");
+
+    // Realizar la solicitud a la API
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            // Actualizar los elementos de la página con los datos de la API
+            bidPriceElement.textContent = data.bid;
+            askPriceElement.textContent = data.ask;
+        })
+        .catch((error) => {
+            console.error("Error al obtener los datos de la API: ", error);
+        });
+});
